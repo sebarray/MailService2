e31ed6afcae0921495c6a60a44aaffc7a661a515
sebarray <sebarray@gmail.com>
2022-09-08 16:04:54 -0300

    tokefromwbe

--- service/mail/OAuth.go
+++ service/mail/OAuth.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -22,15 +21,9 @@ func OAuthGmailService() {
 		Endpoint:     google.Endpoint,
 		RedirectURL:  "http://localhost",
 	}
+	token := getTokenFromWeb(&config)
 
-	token := oauth2.Token{
-		AccessToken:  os.Getenv("ATOKEN"),
-		RefreshToken: os.Getenv("RTOKEN"),
-		TokenType:    "Bearer",
-		Expiry:       time.Now(),
-	}
-
-	var tokenSource = config.TokenSource(context.Background(), &token)
+	var tokenSource = config.TokenSource(context.Background(), token)
 
 	srv, err := gmail.NewService(context.Background(), option.WithTokenSource(tokenSource))
 	if err != nil {
@@ -41,4 +34,21 @@ func OAuthGmailService() {
 	if GmailService != nil {
 		fmt.Println("Email service is initialized ")
 	}
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
 }
